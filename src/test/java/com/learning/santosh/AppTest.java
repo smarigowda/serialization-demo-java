e72289ece7b55b11bb1593374d96b3c43514d430
Santosh Marigowda <santosharakere@gmail.com>
2020-05-25 18:46:49 +0100

    Using RESTAssured to get emoplyee details

--- src/test/java/com/learning/santosh/AppTest.java
+++ src/test/java/com/learning/santosh/AppTest.java
@@ -1,38 +1,13 @@
 package com.learning.santosh;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest {
+    @Test
+    public void GetEmployees() {
+        RestAssured.baseURI = "http://dummy.restapiexample.com";
+        String response = RestAssured.given().when().log().all().get("/api/v1/employees").then().log().all().extract().body().asString();
+        System.out.println(response);
     }
 }
